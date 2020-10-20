023d08d49cfedd2e40038a8586dac44efd48fbfa
Mateusz <matimatik97@gmail.com>
2020-10-20 15:24:07 +0200

    lab 3

--- app/src/main/java/com/example/myfrags/Fragment1.java
+++ app/src/main/java/com/example/myfrags/Fragment1.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -49,16 +50,64 @@ public class Fragment1 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+        Button buttonShuffle = (Button) view.findViewById(R.id.button_shuffle);
+        Button buttonClockwise = (Button) view.findViewById(R.id.button_clockwise);
+        Button buttonHide = (Button) view.findViewById(R.id.button_hide);
+        Button buttonRestore = (Button) view.findViewById(R.id.button_restore);
+
+        buttonShuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) callback.onButtonClickShuffle();
+            }
+        });
+
+        buttonClockwise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) callback.onButtonClickClockwise();
+            }
+        });
+
+        buttonHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) callback.onButtonClickHide();
+            }
+        });
+
+        buttonRestore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) callback.onButtonClickRestore();
+            }
+        });
+
+        return view;
+    }
+
+    //1.
+    public interface OnButtonClickListener {
+        public void onButtonClickShuffle();
+
+        public void onButtonClickClockwise();
+
+        public void onButtonClickHide();
+
+        public void onButtonClickRestore();
+    }
+
+    //2.
+    private OnButtonClickListener callback = null;
+
+    //3.
+    public void setOnButtonClickListener(OnButtonClickListener callback) {
+        this.callback = callback;
     }
 }
